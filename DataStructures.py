a5bdc25a7ed5f480802b493eea74b2f3404078a1
tristenstevens-del <tristen.stevens@hgs.hiddengeniusproject.org>
2026-07-27 12:38:02 -0400

    Create DataStructures.py

---
+++ DataStructures.py
@@ -0,0 +1,30 @@
+1. What are the 4 main built-in data structures in Python?
+   List
+   Tuple
+   Dictionary
+   Set
+
+2. What does it mean for data to be mutable? Immutable?**
+
+   Mutable Data can be changed after it is created.
+   Immutable Data cannot be changed after it is created.
+
+3. What is the difference between a list and a tuple?
+
+   * A **list** is mutable, meaning it can be changed.
+   * A **tuple** is immutable, meaning it cannot be changed.
+
+4. **Pick one of the mutable data structures. How do you add an item to that data structure?**
+
+   * For a **list**, use the `.append()` method to add an item.
+
+5. **How do you access an item in a list or tuple?**
+
+   * Use its index number inside square brackets `[]`.
+
+6. What characters are used to create a list, tuple, dictionary, and set?**
+
+   List: `[]`
+   Tuple: `()`
+   Dictionary `{}`
+   Set `{}`
